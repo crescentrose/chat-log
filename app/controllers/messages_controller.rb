08baef1f86d14be690e62a3c32dc1d913227f725
Ivan Ostric <ivan@halcyon.hr>
2021-08-02 16:51:56 +0200

    Add filters

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,11 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message
+    @q = Message
       .includes(:server)
+      .ransack(params[:q])
+
+    @messages = @q
+      .result(distinct: true)
       .order(:sent_at)
       .page(params[:page])
   end
