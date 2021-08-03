a14cf98f96af5d18c42bc5bf37ea61678bb444fb
Ivan Ostric <ivan@halcyon.hr>
2021-08-03 14:38:13 +0200

    cover empty chat edge case

--- app/jobs/parse_log_file_job.rb
+++ app/jobs/parse_log_file_job.rb
@@ -8,8 +8,9 @@ class ParseLogFileJob < ApplicationJob
     messages = parser
       .parse_blob(log_file.file)
       .map { |message| message.to_h.merge(server_id: server_id, created_at: Time.now, updated_at: Time.now) }
+      .compact
 
-    Message.insert_all(messages)
+    Message.insert_all(messages) unless messages.empty?
   ensure
     log_file.update(processed: true)
   end
