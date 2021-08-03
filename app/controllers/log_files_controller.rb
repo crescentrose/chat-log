57c822b7ba5506f9d1f10fec6a05234d9347228e
Ivan Ostric <ivan@halcyon.hr>
2021-08-03 23:32:31 +0200

    Add basic auth for log file upload

--- app/controllers/log_files_controller.rb
+++ app/controllers/log_files_controller.rb
@@ -1,4 +1,5 @@
 class LogFilesController < ApplicationController
+  before_action :authenticate
   skip_before_action :verify_authenticity_token
 
   def create
@@ -22,5 +23,13 @@ class LogFilesController < ApplicationController
         server: Server.from_name(params[:log_file][:server_name]),
         processed: false
       )
+  end
+
+  def authenticate
+    return unless ENV['LOG_UPLOAD_PASSWORD'].present?
+
+    authenticate_or_request_with_http_token do |token, options|
+      ActiveSupport::SecurityUtils.secure_compare(token, ENV['LOG_UPLOAD_PASSWORD'])
+    end
   end
 end
