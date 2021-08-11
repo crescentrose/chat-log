5b76bc56f5b2ee804d8152b578e74c167bff5394
Ivan Ostric <ivan@halcyon.hr>
2021-08-12 00:07:35 +0200

    More stylish messages overview

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -6,4 +6,9 @@ module MessagesHelper
   def steam_url(steam_id64)
     "https://steamcommunity.com/profiles/#{steam_id64}"
   end
+
+  def server_tag(server)
+    hue = server.id * (360 / 15) # TODO: customizable server colours
+    content_tag(:span, server.name, class: 'badge', style: "background-color: hsl(#{hue}, 40%, 40%)")
+  end
 end
