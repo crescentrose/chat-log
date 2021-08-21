c63d410c5b77dec947455d02773cdd5508a60a4a
Ivan Ostric <ivan@halcyon.hr>
2021-08-21 19:40:05 +0200

    Better error handling in server updater

--- app/jobs/update_server_status_job.rb
+++ app/jobs/update_server_status_job.rb
@@ -5,6 +5,9 @@ class UpdateServerStatusJob < ApplicationJob
     Server.where.not(rcon_password: [nil, '']).find_each do |server|
       Sidekiq.logger.info "Updating server #{server.name}"
       ServerStatusUpdateService.new.update(server)
+    rescue StandardError => e
+      Sidekiq.logger.error "Error updating server #{server.name}:"
+      Sidekiq.logger.error e.message
     end
   end
 end
