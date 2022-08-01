ad07a82dc20b5b9b57d225516ca5bf58fffc1d1f
Ivan Oštrić <ivan@halcyon.hr>
2022-08-01 23:15:19 +0200

    camp3r special

--- app/controllers/connection_events_controller.rb
+++ app/controllers/connection_events_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class ConnectionEventsController < ApplicationController
   def index
     authorize ConnectionEvent
@@ -7,10 +9,22 @@ class ConnectionEventsController < ApplicationController
       .ransack(params[:q])
 
     @connection_events = @q
-      .result(distinct: true)
+      .result
       .order(connected_at: :desc)
       .page(params[:page])
-      .per(50)
+      .per(100)
+      .without_count
+
+    respond_to do |format|
+      format.csv do
+        response.headers['Content-Type'] = 'text/csv'
+        response.headers['Content-Disposition'] = 'attachment;filename=players.csv'
+        render :index
+      end
+      format.html do
+        render :index
+      end
+    end
   rescue NotImplementedError => e
     flash[:error] = e.message
     redirect_to votekick_events_path
